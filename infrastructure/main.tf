cdab0682645ffc79e98b633f8e6d05b6f535b33d
ollie-small <ollie.small@kainos.com>
2025-11-18 16:41:58 +0000

    Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,7 +30,7 @@ data "azurerm_container_app_environment" "platform_env" {
 
 # User-Assigned Managed Identity for Container App
 resource "azurerm_user_assigned_identity" "container_identity" {
-  name                = "${var.app_name}"
+  name                = var.app_name
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
 
