c303c7a352c88e782c5b5c07844d23ea3cae63a3
ollie-small <ollie.small@kainos.com>
2025-11-19 10:54:33 +0000

    Fixed the migrations
    We have changed the migragtion stuff to work better with the docker image creation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 import { jobScheduler } from "./di/container.js";
+import { runMigrationsWithRetry } from "./db/runMigrations.js";
+
+// Run migrations on startup in production (non-blocking)
+// In development, migrations should be run manually via `npm run db:migrate`
+if (env.nodeEnv === "production") {
+  runMigrationsWithRetry();
+}
 
 app.listen(env.port, () => {
   console.log(`Server is running on http://localhost:${env.port}`);
