2b77c49b68f309fe00d71a58c492a5bb219902db
ollie-small <ollie.small@kainos.com>
2025-11-19 11:43:42 +0000

    Fixed biome

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
-import { jobScheduler } from "./di/container.js";
 import { runMigrationsWithRetry } from "./db/runMigrations.js";
+import { jobScheduler } from "./di/container.js";
 
 // Run migrations on startup in production (non-blocking)
 // In development, migrations should be run manually via `npm run db:migrate`
